ca8d76b0df32d862d7fef86a4d3d151b3ab30748
Andrem <andreaif4495@gmail.com>
2019-04-15 19:34:23 -0400

    first commit

---
+++ client/config/index.js
@@ -0,0 +1,3 @@
+const MONGODB_URI = 'mongodb://heroku_dr6txt9p:lb00ckojbubl391ks8vsj76e3m@ds139956.mlab.com:39956/heroku_dr6txt9p';
+//const MONGODB_URI = 'mongodb://localhost/books';
+module.exports = MONGODB_URI;
